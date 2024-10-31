0c0fd79a8ce4c8a5013cf70b8a62c5fdfff640f8
Raffaello Bonghi <raffaello@rnext.it>
2024-10-31 11:26:44 +0100

    Fix test first release

--- src/nanosaur/__init__.py
+++ src/nanosaur/__init__.py
@@ -29,5 +29,5 @@ __cr__ = "(c) 2024, RB"
 __copyright__ = "(c) 2024, Raffaello Bonghi"
 # Version package
 # https://packaging.python.org/guides/distributing-packages-using-setuptools/#choosing-a-versioning-scheme
-__version__ = "2.0.0"
+__version__ = "0.0.1"
 # EOF
